dfea353856be8e2ff56f9aad384400f2181d44db
ampatte <ampatterson1@hotmail.com>
2023-04-15 21:38:54 -0400

    removed duplicate code, renamed a few variables, rewrote conditional statements

--- script.js
+++ script.js
@@ -1,50 +1,62 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var pwdLength = (length >=8 <=128);
-//var charTypes = [uppercaseChar, lowercaseChar, numericChar, specialChar];
-var lowercaseChar = "abcdefghijklmnopqrstuvwxyz";
-var uppercaseChar = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
-var numericChar = "0123456789".split('');
-var specialChar = ("!","@","#","$","%","^","&");
-//create 4 arrays w characters //arrayvar uppercaseChar = ;
-//upperCase,lowerCase,numeric,special
+var lowLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var upLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","W","X","Y","Z"];
+var numeric = [0,1,2,3,4,5,6,7,8,9];
+var special = ["!","@","#","$","%","^","&"];
 
-function pwdOptions(){ 
-    //how long is pwd
-    let pwdLength = prompt("Choose a length between 8 and 128 characters.");
-      console.log(pwdLength);
-        return pwdLength;}
-    
-    var lowercase = confirm ("Would you like to include lowercase characters?");
-      
-    var uppercase = confirm("Would you like to include uppercase characters?");
-
-    var numeric= confirm("Would you like to include numbers?"); 
-
-    var special = confirm("Would you like to include special characters?");    
-    
-     // if (lowercase===true) {
-      
-/*function name(params) {
-    return param; 
-}*/
 
 function generatePassword() {
-  let options = pwdOptions( 
   
-  )
-  return Math.floor(Math.random()*uppercaseChar.length)
-  //console.log (generatePassword)
+  var pwdLength = prompt("Choose a length between 8 and 128 characters.");
+
+  if (pwdLength < 8 ) {
+    alert("password must be greater than 8 characters");
+      return;
+  } else if (pwdLength > 128) {
+      alert("password must be less than 128 characters");
+        return;
+    } 
+
+  var passwordText = "";
+
+//var confirm = true;
+  var includeLowLetters = confirm ("Would you like to include lowercase letters?");
+  if (includeLowLetters === true) {
+    passwordText += lowLetters.join(''); 
+  }
+  var includeUpLetters = confirm ("Would you like to include uppercase letters?");
+  if (includeUpLetters === true) {
+      passwordText += upLetters.join('');
+    }
+  var includeNumeric = confirm("Would you like to include numbers?"); 
+  if (includeNumeric === true) {
+      passwordText += numeric.join('');
+    }
+  var includeSpecial = confirm("Would you like to include special characters?");  
+  if (includeSpecial === true) {
+        passwordText += special.join('');
+      }
+
+  var password = "";
+  
+  for (var i = 0; i < pwdLength; i++) {
+    var randomChar = Math.floor(Math.random() * passwordText.length)
+    password += passwordText[randomChar];
+  
 }
+  return password;
+}
+ 
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  var pwdArray= Array(length);
+  //var pwdArray = Array(pwdLength);
   passwordText.value = password;
-for (i = 8; i< length; i++){
-}
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
